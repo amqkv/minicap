b951e8caa2774977b2f48c03aaccf4d4e83aa036
Kelvin <kelvintrang2000@gmail.com>
2022-01-23 17:19:00 -0500

    Did login page with yip and navbar

---
+++ frontend/components/NavBar.tsx
@@ -0,0 +1,121 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  Heading,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  Stack,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import NextLink from "next/link";
+import { mainColor, websiteName } from "@frontend/utils/constants";
+
+interface NavLinkProps {
+  children?: ReactNode;
+  url: string;
+}
+
+const Links = [
+  { text: "HOME", url: "#" },
+  { text: "ABOUT", url: "#" },
+  { text: "CONTACT", url: "#" },
+];
+
+const NavLink = ({ children, url }: NavLinkProps) => (
+  <NextLink href={url} passHref>
+    <Link
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "underline",
+        textDecorationColor: mainColor,
+      }}
+    >
+      {children}
+    </Link>
+  </NextLink>
+);
+
+export default function Simple() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Flex
+            alignItems={"center"}
+            width={"100%"}
+            justifyContent={"space-between"}
+          >
+            <Menu>
+              {/* <MenuButton
+                as={Button}
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
+                minW={0}
+              > */}
+              <Flex>
+                <Avatar size={"md"} src={"https://i.imgur.com/nBBi6wd.png"} />
+                <Heading as="h3" size="lg" alignSelf={"center"}>
+                  {websiteName}
+                </Heading>
+              </Flex>
+              {/* </MenuButton> */}
+              <HStack spacing={8} alignItems={"flex-end"}>
+                <HStack
+                  as={"nav"}
+                  spacing={4}
+                  display={{ base: "none", md: "flex" }}
+                >
+                  {Links.map(({ text, url }) => (
+                    <NavLink key={text} url={url}>
+                      {text}
+                    </NavLink>
+                  ))}
+                </HStack>
+              </HStack>
+              {/* <MenuList>
+                <MenuItem>Link 1</MenuItem>
+                <MenuItem>Link 2</MenuItem>
+                <MenuDivider />
+                <MenuItem>Link 3</MenuItem>
+              </MenuList> */}
+            </Menu>
+          </Flex>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map(({ text, url }) => (
+                <NavLink key={text} url={url as any}>
+                  {text}
+                </NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,9 @@
-import { useSession, signIn, signOut, getSession } from 'next-auth/react';
-import { GetServerSideProps } from 'next';
-import LoginLogoutButton from '@frontend/components/LoginLogoutButton'
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
+import LoginLogoutButton from "@frontend/components/LoginLogoutButton";
+import { Flex, Heading, Container } from "@chakra-ui/react";
 
-export const getServerSideProps: GetServerSideProps = async context => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     props: {
       session: await getSession(context),
@@ -18,14 +19,32 @@ export default function Home() {
       <>
         Signed in as {session.user.name} <br />
         type: {session.user.type} <br />
-        <LoginLogoutButton/>
+        <LoginLogoutButton />
       </>
     );
   }
   return (
+
     <>
-      Not signed in <br />
-      <LoginLogoutButton />
+      <Flex position={"relative"} width={"50%"} height={"100%"}>
+        <Container>
+          TODO
+        </Container>
+      </Flex>
+
+      <Flex
+        width={"50%"}
+        height={"100%"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        display={"flex"}
+      >
+        <Flex direction="column" background="gray.100" p={20} rounded={6}>
+          <Heading mb={6}>Login to your account</Heading>
+
+          <LoginLogoutButton />
+        </Flex>
+      </Flex>
     </>
   );
 }
