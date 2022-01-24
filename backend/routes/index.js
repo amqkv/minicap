50806b06aa5f164e61be44f86657f47326815195
Jean-Francois Vo <jef.anhduc@gmail.com>
2022-01-24 00:03:57 -0500

    refactor database config connection and backend restructure

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const sql = require("../dboperation");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,20 +8,6 @@ router.get('/', function(req, res, next) {
 });
 
 
-//test connection
-router.get('/testconnection', function(req, res, next) {
-  sql.getdata();
-  res.render('index', { title: 'Express' });
-});
-
-
-router.get("/getdata_withQuery", function (req, res, next) {
-  sql.getdata_withQuery().then((result) => {
-    res.json(result[0]);
-  });
-});
-
-
 module.exports = {
     router: router
-  };
+};
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../config/database');
+const User = require('../models/User');
+
 router.use(express.json());
 
+//get all users inside the database
+router.get('/', (req,res) => 
+  User.findAll()
+    .then(users => {
+      console.log(users);
+      res.json(users);
+    })
+    .catch(err => console.log(err))
+);
 
+//login
 router.post('/login', (req, res) => {
     const { username, password } = req.body;
   
