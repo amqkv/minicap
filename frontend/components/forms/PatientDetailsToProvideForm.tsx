7da17c7208f0d5a7d260cf81b21d52d0660b4597
Be An <amqkvo@gmail.com>
2022-01-27 14:04:45 -0500

    Completed frontend form for required details to provide from patients

--- frontend/components/forms/PatientDetailsToProvideForm.tsx
+++ frontend/components/forms/PatientDetailsToProvideForm.tsx
@@ -1,16 +1,52 @@
-import { Checkbox, Stack } from '@chakra-ui/react';
+import { Checkbox, Stack, Button, CheckboxGroup, extendTheme, Flex, Box } from '@chakra-ui/react';
+
+// <TODO> Fetch details required from backend
+let detailsRequired = [{detail: 'Weight', required: true}, {detail: 'Temperature', required: true}, {detail: 'Symptoms', required: true}, {detail: 'Chronic Illnesses', required: false}]
+let updatedState = detailsRequired
+
+function getCheckedBoxes(){
+    let requiredDetails = []
+    for(let i = 0; i < detailsRequired.filter(elm => elm.required == true).length; i++){
+        requiredDetails.push(detailsRequired.filter(elm => elm.required == true)[i].detail)
+    }
+    return requiredDetails
+}
+
+function setCheckedItems(detail: string, newCheckedState : boolean){
+    updatedState[updatedState.findIndex(elm => elm.detail == detail)].required = newCheckedState
+}
+
+function onSave(){
+    // <TODO> - send updated state to the backend
+    console.log(updatedState)
+}
 
 export default function PatientDetailsToProvideForm(){
-    // <TODO> Fetch details required from backend
-    let detailsRequired = [{detail: 'Weight', required: true}, {detail: 'Temperature', required: true}, {detail: 'Symptoms', required: true}, {detail: 'Chronic Illnesses', required: false}]
     let checkboxes = []
     for(let i = 0; i < detailsRequired.length; i++){
-        checkboxes.push(<Checkbox isChecked={detailsRequired[i].required} colorScheme='red'> {detailsRequired[i].detail} </Checkbox>)
+        checkboxes.push(
+        <Checkbox 
+            key={detailsRequired[i].detail}
+            value={detailsRequired[i].detail}
+            colorScheme='red'
+            onChange={(e) => setCheckedItems(detailsRequired[i].detail, e.target.checked)}
+            > 
+                {detailsRequired[i].detail} 
+        </Checkbox>)
     }
 
     return(
-        <Stack spacing={5} direction='row'>
-            {checkboxes}
-        </Stack>
+        <div className='patient-details_form'>
+            <Stack spacing={5} direction='row'>
+                <CheckboxGroup defaultValue={getCheckedBoxes()}>
+                    {checkboxes}
+                </CheckboxGroup>
+            </Stack>
+            <Flex justify='flex-end'>
+                <Button colorScheme='red' onClick={onSave}>Save</Button>
+            </Flex>
+        </div>
+
+
     )
 }
--- frontend/pages/doctor/patientinfo.tsx
+++ frontend/pages/doctor/patientinfo.tsx
@@ -1,60 +1,67 @@
 import PatientDetailsToProvideForm from '@frontend/components/forms/PatientDetailsToProvideForm';
-import { Box, Flex, Heading, Text, Image, Divider} from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, Image, Divider } from '@chakra-ui/react';
 
 let patient = {
-    basicInformation: {
-        firstName: 'Dragun',
-        lastName: 'Di Zenuts',
-        gender: 'Male',
-        age: 62,
-        admissionDate: '01/01/2022',
+  basicInformation: {
+    firstName: 'Dragun',
+    lastName: 'Di Zenuts',
+    gender: 'Male',
+    age: 62,
+    admissionDate: '2022-01-01',
+  },
+  conditionDetails: [
+    {
+      updateDate: '2022-01-22',
+      temperature: 37.6,
+      weight: 132,
+      symptoms: ['cough', 'sore throat', 'chest pains'],
     },
-    conditionDetails: {
-        statusTime: '13h',
-        temperature: 37.6,
-        weight: 132,
-        symptoms: ['cough', 'sore throat', 'chest pains']
-    }
-}
+  ],
+};
 
-// function getPatientCondition(){
-//     let details = []
-//     for(let i = 1; i < patient.conditionDetails.length; i ++){
-//         details.push
-//     }
-// }
-
-export default function PatientInfo(){
-
-    return(
-        <div className='patient-info'>
-        <div className='section basic-details'>
-            <Flex>
-                <Box >
-                    <Image src='https://static.thenounproject.com/png/389195-200.png' alt='Patient Picture' boxSize='140px'/>
-                </Box>
-                <Box >
-                    <Heading size='lg'>{patient.basicInformation.firstName + ' ' + patient.basicInformation.lastName}</Heading>
-                    <Text fontSize='xl'>{patient.basicInformation.gender}</Text>
-                    <Text fontSize='xl'>Age: {patient.basicInformation.age}</Text>
-                    <Text fontSize='xl'>Admitted On: {patient.basicInformation.admissionDate}</Text>
-                </Box>
-            </Flex>
+export default function PatientInfo() {
+  return (
+    <div className="patient-info">
+      <div className="section basic-details">
+        <Flex>
+          <Box flex="1">
+            <Image
+              src="https://static.thenounproject.com/png/389195-200.png"
+              alt="Patient Picture"
+              boxSize="140px"
+              width="190px"
+            />
+          </Box>
+          <Box flex="3.5">
+            <Heading size="lg">
+              {patient.basicInformation.firstName +
+                ' ' +
+                patient.basicInformation.lastName}
+            </Heading>
+            <Text fontSize="xl">{patient.basicInformation.gender}</Text>
+            <Text fontSize="xl">
+              <b>Age: </b>
+              {patient.basicInformation.age}
+            </Text>
+            <Text fontSize="xl">
+              <b>Admitted On:</b> {patient.basicInformation.admissionDate}
+            </Text>
+          </Box>
+        </Flex>
+      </div>
+      <Divider />
+      <div className="section desired-details">
+        <div className="header">
+          <Heading size="lg"> Desired Details</Heading>
         </div>
-        <Divider/>
-        <div className='section desired-details'>
-            <div className='header'>
-                <Heading size='lg'> Desired Details</Heading>
-            </div>
-            <div className='form-container'>
-                <PatientDetailsToProvideForm/>
-            </div>
+        <div className="form-container">
+          <PatientDetailsToProvideForm />
         </div>
-        <Divider/>
-        <div className='section updated-details'>
-            <Heading size='lg'>Details updated {patient.conditionDetails.statusTime} ago:</Heading>
-            <Text></Text>
-        </div>
-        </div>
-    )
+      </div>
+      <Divider />
+      <div className="section updated-details">
+        {/* <TODO> Insert updated detals history */}
+      </div>
+    </div>
+  );
 }
