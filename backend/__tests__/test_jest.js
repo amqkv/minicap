5f9d79dac9a233a0c95a9470deaa9702d9f1a7de
Olivier <olivier_phan@outlook.com>
2022-01-30 03:43:05 -0500

    SOEN-57: extra dummy test to test node cache

--- backend/__tests__/test_jest.js
+++ backend/__tests__/test_jest.js
@@ -2,3 +2,7 @@ test('two plus two is four', () => {
     //just making sure jest actually runs
     expect(2 + 2).toBe(4);
 });
+
+test('quick maths', () => {
+    expect('quick' + ' maths').toBe('quick maths');
+});
