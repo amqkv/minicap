35f99c63aae8123b6d470fae8389268121e0b7f4
Be An <amqkvo@gmail.com>
2022-01-29 22:01:36 -0500

    Modified PR template

--- backend/index.js
+++ backend/index.js
@@ -36,9 +36,6 @@ app.use('/patients', patientsRouter);
 // CORS headers
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  // res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
