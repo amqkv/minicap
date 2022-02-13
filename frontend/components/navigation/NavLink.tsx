e6ec368f184eb11ac6e4827528d9592885c6649b
NicholasYip <yiphoiye.n@gmail.com>
2022-02-12 19:56:23 -0500

    test attempt 7

--- frontend/components/navigation/NavLink.tsx
+++ frontend/components/navigation/NavLink.tsx
@@ -1,7 +1,7 @@
-import { Link } from '@chakra-ui/react';
-import { ReactNode } from 'react';
-import NextLink from 'next/link';
-import { MAIN_COLOR } from '@frontend/utils/constants';
+import { Link } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import NextLink from "next/link";
+import { MAIN_COLOR } from "@frontend/utils/constants";
 
 interface NavLinkProps {
   children?: ReactNode;
@@ -11,13 +11,13 @@ interface NavLinkProps {
 
 const NavLink = ({ children, url, textDecoration }: NavLinkProps) => {
   const hoverStyling = {
-    textDecoration: textDecoration ? 'underline' : 'none',
+    textDecoration: textDecoration ? "underline" : "none",
     textDecorationColor: MAIN_COLOR,
   };
 
   return (
     <NextLink href={url} passHref>
-      <Link px={2} py={1} rounded={'md'} _hover={hoverStyling}>
+      <Link px={2} py={1} rounded={"md"} _hover={hoverStyling}>
         {children}
       </Link>
     </NextLink>
