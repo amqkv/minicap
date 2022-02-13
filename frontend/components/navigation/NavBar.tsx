16383c683b43e78d40bdec1daa80744ca4a2f1db
NicholasYip <yiphoiye.n@gmail.com>
2022-02-12 19:01:04 -0500

    test attempt 7

--- frontend/components/navigation/NavBar.tsx
+++ frontend/components/navigation/NavBar.tsx
@@ -13,12 +13,12 @@ import {
   MenuDivider,
   useDisclosure,
   Stack,
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
-import NavLink from '@frontend/components/navigation/navlink';
-import { useSession } from 'next-auth/react';
-import Logo from '@frontend/components/navigation/logo';
-import { links, link } from '@frontend/components/navigation/navbar-structure';
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import NavLink from "@frontend/components/navigation/navlink";
+import { useSession } from "next-auth/react";
+import Logo from "@frontend/components/navigation/logo";
+import { links, link } from "@frontend/components/navigation/navbar-structure";
 
 export default function Navbar() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -28,8 +28,12 @@ export default function Navbar() {
   return (
     <>
       <Box as="header" px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Flex alignItems={'center'} width={'100%'} justifyContent={'space-between'}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Flex
+            alignItems={"center"}
+            width={"100%"}
+            justifyContent={"space-between"}
+          >
             <Menu>
               {/* <MenuButton
                 as={Button}
@@ -40,10 +44,15 @@ export default function Navbar() {
               > */}
               <Logo />
               {/* </MenuButton> */}
-              <HStack spacing={8} alignItems={'flex-end'}>
-                <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+              <HStack spacing={8} alignItems={"flex-end"}>
+                <HStack
+                  as={"nav"}
+                  spacing={4}
+                  display={{ base: "none", md: "flex" }}
+                >
                   {links.map(({ text, url, roleRequired }: link) => {
-                    const renderLink = roleRequired === userRole || !roleRequired;
+                    const renderLink =
+                      roleRequired === userRole || !roleRequired;
                     return (
                       renderLink && (
                         <NavLink textDecoration key={text} url={url}>
@@ -57,17 +66,17 @@ export default function Navbar() {
             </Menu>
           </Flex>
           <IconButton
-            size={'md'}
+            size={"md"}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
           />
         </Flex>
 
         {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
               {links.map(({ text, url, roleRequired }: link) => {
                 const renderLink = roleRequired === userRole || !roleRequired;
 
