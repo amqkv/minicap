663632bb4e6bc5a6fa76f428bd320487ea02beb1
Jean-Francois Vo <jef.anhduc@gmail.com>
2022-02-16 09:51:36 -0500

    bug fix for tests in assign patient to doctor functionality due to changes in db

--- backend/__tests__/routes/admins.test.js
+++ backend/__tests__/routes/admins.test.js
@@ -12,7 +12,7 @@ describe("PATCH: Assign a patient to a doctor", () => {
     it("Patient has been assigned to a doctor", () => {
         let data = {
             accountId: "17",
-            patientId: "13",
+            patientId: "3",
             doctor_doctorId: "3",
         };
         return request(app).patch("/admins/assign-patient-doctor").send(data).expect(200);
@@ -21,7 +21,7 @@ describe("PATCH: Assign a patient to a doctor", () => {
     it("PatientId not found", () => {
         let data = {
             accountId: "17",
-            patientId: "1",
+            patientId: "0",
             doctor_doctorId: "3",
         };
         return request(app).patch("/admins/assign-patient-doctor").send(data).expect(400);
@@ -30,7 +30,7 @@ describe("PATCH: Assign a patient to a doctor", () => {
     it("DoctorId not found", () => {
         let data = {
             accountId: "17",
-            patientId: "13",
+            patientId: "3",
             doctor_doctorId: "0",
         };
         return request(app).patch("/admins/assign-patient-doctor").send(data).expect(400);
@@ -39,7 +39,7 @@ describe("PATCH: Assign a patient to a doctor", () => {
     it("User is not signed in", () => {
         let data = {
             accountId: "0",
-            patientId: "13",
+            patientId: "3",
             doctor_doctorId: "3",
         };
         return request(app).patch("/admins/assign-patient-doctor").send(data).expect(403);
@@ -47,8 +47,8 @@ describe("PATCH: Assign a patient to a doctor", () => {
 
     it("AccountId is not an admin", () => {
         let data = {
-            accountId: "87",
-            patientId: "13",
+            accountId: "51",
+            patientId: "3",
             doctor_doctorId: "3",
         };
         return request(app).patch("/admins/assign-patient-doctor").send(data).expect(401);
