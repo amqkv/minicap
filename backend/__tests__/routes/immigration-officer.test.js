dde97ed53e94a1d2c152c0cfea93b470d84eb154
unknown <steven.vuong00@gmail.com>
2022-02-21 15:05:28 -0500

    fixed backend test

--- backend/__tests__/routes/immigration-officer.test.js
+++ backend/__tests__/routes/immigration-officer.test.js
@@ -6,14 +6,14 @@ beforeAll(() => {
     // test DB
     db.authenticate();
 });
-afterAll(() => {
+afterAll(async () => {
     // Closing the DB connection allows Jest to exit successfully.
-    db.close();
+    await db.close();
 });
 
 describe("GET:list of patients from the database", () => {
     it("Returns code 200 after querying the list of patients", () => {
-        request(app)
+        return request(app)
             .get("/immigration-officer/findUsersStatus")
             .expect("Content-Type", /json/)
             .expect(200)
@@ -21,7 +21,7 @@ describe("GET:list of patients from the database", () => {
                 expect(response.body).toEqual(
                     expect.arrayContaining([
                         expect.objectContaining({
-                            hasCovid: expect.any(Number),
+                            hasCovid: expect.any(Boolean),
                             firstName: expect.any(String),
                             lastName: expect.any(String),
                             gender: expect.any(String),
