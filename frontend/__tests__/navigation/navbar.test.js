acbfc7c38c0d7d1987fcbb0b917c42616f6fe18a
Olivier <olivier_phan@outlook.com>
2022-02-23 18:18:41 -0500

    SOEN-82: Adding more tests

--- frontend/__tests__/navigation/navbar.test.js
+++ frontend/__tests__/navigation/navbar.test.js
@@ -5,51 +5,69 @@ import Logo from "@frontend/components/navigation/logo";
 import NavLink from "@frontend/components/navigation/navlink";
 import LoginLogoutButton from "@frontend/components/login-logout-button";
 import { USER_ROLES } from "@frontend/utils/constants";
+import { IconButton, Stack } from "@chakra-ui/react";
 
 jest.mock("next-auth/react");
+// Using DOCTOR ROLE to test standard rendering.
+const numberOfDoctorNavLinks = 2;
 
 describe("<Navbar />", () => {
-  it("renders a <Logo /> component and a Home <NavLink> when the user isn't logged in", () => {
-    useSession.mockReturnValue({});
+    it("renders a <Logo /> component and a Home <NavLink> when the user isn't logged in", () => {
+        useSession.mockReturnValue({});
 
-    const wrapper = shallow(<NavBar />);
-    expect(wrapper.find(Logo)).toHaveLength(1);
-    expect(wrapper.find(NavLink)).toHaveLength(1);
-    expect(wrapper.find(LoginLogoutButton)).toHaveLength(1);
-
-  });
-
-  it("renders a <Logo /> component and a 4 <NavLink> when the user is an admin", () => {
-    useSession.mockReturnValue({
-      data: {
-        user: {
-          Role: USER_ROLES.admin,
-        },
-      },
+        const wrapper = shallow(<NavBar />);
+        expect(wrapper.find(Logo)).toHaveLength(1);
+        expect(wrapper.find(NavLink)).toHaveLength(1);
+        expect(wrapper.find(LoginLogoutButton)).toHaveLength(1);
     });
 
+    it("renders a <Logo /> component and a 4 <NavLink> when the user is an admin", () => {
+        useSession.mockReturnValue({
+            data: {
+                user: {
+                    Role: USER_ROLES.admin,
+                },
+            },
+        });
 
-    const wrapper = shallow(<NavBar />);
-    expect(wrapper.find(Logo)).toHaveLength(1);
-    expect(wrapper.find(NavLink)).toHaveLength(4);
-    expect(wrapper.find(LoginLogoutButton)).toHaveLength(1);
-
-  });
-
-  it("renders a <Logo /> component and a 2 <NavLink> when the user is a doctor", () => {
-    useSession.mockReturnValue({
-      data: {
-        user: {
-          Role: USER_ROLES.doctor,
-        },
-      },
+        const wrapper = shallow(<NavBar />);
+        expect(wrapper.find(Logo)).toHaveLength(1);
+        expect(wrapper.find(NavLink)).toHaveLength(4);
+        expect(wrapper.find(LoginLogoutButton)).toHaveLength(1);
     });
 
+    it("renders a <Logo /> component and a " + numberOfDoctorNavLinks + " <NavLink> when the user is a doctor", () => {
+        useSession.mockReturnValue({
+            data: {
+                user: {
+                    Role: USER_ROLES.doctor,
+                },
+            },
+        });
 
-    const wrapper = shallow(<NavBar />);
-    expect(wrapper.find(Logo)).toHaveLength(1);
-    expect(wrapper.find(NavLink)).toHaveLength(2);
-    expect(wrapper.find(LoginLogoutButton)).toHaveLength(1);
+        const wrapper = shallow(<NavBar />);
+        expect(wrapper.find(Logo)).toHaveLength(1);
+        expect(wrapper.find(NavLink)).toHaveLength(numberOfDoctorNavLinks);
+        expect(wrapper.find(LoginLogoutButton)).toHaveLength(1);
+    });
 
-  });
+    it("Open and close the navbar menu and test element rendering", () => {
+        useSession.mockReturnValue({
+            data: {
+                user: {
+                    Role: USER_ROLES.doctor,
+                },
+            },
+        });
+        let wrapper = shallow(<NavBar />);
+		// Icon renders
+		expect(wrapper.find(IconButton)).toHaveLength(1);
+		expect(wrapper.find(Stack)).toHaveLength(0);
+		console.log(wrapper.debug());
+		
+		wrapper.find(IconButton).props().onClick();
+		console.log(wrapper.debug());
+		expect(wrapper.find(Stack)).toHaveLength(1);
+		expect(wrapper.find(NavLink)).toHaveLength(numberOfDoctorNavLinks * 2);
+    });
 });
--- frontend/__tests__/pages/immigration-officer/find-users.test.js
+++ frontend/__tests__/pages/immigration-officer/find-users.test.js
@@ -1,13 +1,13 @@
 import { useSession } from "next-auth/react";
-import { mount, shallow } from "enzyme";
+import { shallow } from "enzyme";
 import { USER_ROLES } from "@frontend/utils/constants";
 import { IMMIGRATION_OFFICER_MOCK_PATIENTS } from "@frontend/__tests__/__mock__/mock";
-import UserList, { filterByText } from "@frontend/pages/immigration-officer/find-users";
+import UserList, { filterByText, getServerSideProps } from "@frontend/pages/immigration-officer/find-users";
 import Circle from "@frontend/components/circle";
 import List from "@frontend/components/admin/list";
 import Modal from "@frontend/components/modal";
 import Legend from "@frontend/components/legend";
-import { Input, Image } from "@chakra-ui/react";
+import { Input, Image, Flex } from "@chakra-ui/react";
 
 jest.mock("next-auth/react");
 
@@ -40,6 +40,35 @@ describe("immigration officer find users page", () => {
         expect(wrapper.find(Modal)).toHaveLength(1);
         expect(wrapper.find(Circle)).toHaveLength(2);
         expect(wrapper.find(Image)).toHaveLength(1);
+    });
+    it("Click on a user and opens modal", () =>{
+        useSession.mockReturnValue({
+            data: {
+                user: {
+                    Role: USER_ROLES.iOfficer,
+                },
+            },
+        });
+        const wrapper = shallow(<UserList patients={IMMIGRATION_OFFICER_MOCK_PATIENTS} />);
+        expect(wrapper.find(Modal).prop("isOpen")).toBeFalsy();
+        // index 0 is Flex for search input.
+        const userRow = shallow(wrapper.find(Flex).get(1));
+        userRow.props().onClick();
+        wrapper.update();
+        expect(wrapper.find(Modal).prop("isOpen")).toBeTruthy();
+    });
+    it("Simulate onChange event on the filter bar", () => {
+        useSession.mockReturnValue({
+            data: {
+                user: {
+                    Role: USER_ROLES.iOfficer,
+                },
+            },
+        });
+        const wrapper = shallow(<UserList patients={IMMIGRATION_OFFICER_MOCK_PATIENTS} />);
+        const inputBox = shallow(wrapper.find(Input).get(0));
+        inputBox.simulate("change", { target: {value: "a"}});
+        
     });
 });
 
