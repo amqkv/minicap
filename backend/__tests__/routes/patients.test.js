9bf74eac5f196cf61e2c1ca84df8d96790e444e9
Olivier <olivier_phan@outlook.com>
2022-02-23 19:49:40 -0500

    soen-82: patient test coverage

---
+++ backend/__tests__/routes/patients.test.js
@@ -0,0 +1,26 @@
+const request = require("supertest");
+const app = require("../../index");
+const db = require("../../config/database");
+const { TEST_CONSTANTS } = require("../../utils/constants");
+
+beforeAll(() => {
+    // test DB
+    db.authenticate();
+});
+afterAll(() => {
+    // Closing the DB connection allows Jest to exit successfully.
+    db.close();
+});
+
+
+describe("Test get Patient details route ", () => {
+    it("connect to /getRequiredDetails/:AccountId with valid patient id", () => {
+        const url = "/patients/getRequiredDetails/" + TEST_CONSTANTS.PATIENT_ACCOUNT.AccountId;
+        return request(app).get(url).send().expect(200); 
+    });
+    //@todo ERROR CATCHING IN THIS ROUTE
+    it("connect to /getRequiredDetails/:AccountId with an invalid id", () => {
+        const url = "/patients/getRequiredDetails/" + TEST_CONSTANTS.PATIENT_ACCOUNT.AccountId;
+        return expect(true).toBe(true);
+    });
+});
