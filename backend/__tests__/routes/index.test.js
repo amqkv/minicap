1570fa4a33a80e7f55557174978a1fa9467bcf74
Olivier <olivier_phan@outlook.com>
2022-02-23 20:28:32 -0500

    trying to fix CI

--- backend/__tests__/routes/index.test.js
+++ backend/__tests__/routes/index.test.js
@@ -2,6 +2,8 @@ const request = require("supertest");
 const app = require("../../index");
 const db = require("../../config/database");
 
+jest.useFakeTimers()
+
 beforeAll(() => {
     // test DB
     db.authenticate();
