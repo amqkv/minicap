8a526c094be5e9b20fe14d1b561b0ca62e88f25c
NicholasYip <yiphoiye.n@gmail.com>
2022-03-10 15:24:05 -0500

    renaming

--- frontend/__tests__/homepage/dashboard-charts.test.js
+++ frontend/__tests__/homepage/dashboard-charts.test.js
@@ -4,13 +4,13 @@ import { shallow } from "enzyme";
 
 import { BarChart, CartesianGrid, YAxis, Tooltip, Legend, Bar } from "recharts";
 
-describe("test the dashboard <Dashboard/>", () => {
-    it("does not renders the dashboard", () => {
+describe("test the dashboard <DashboardCharts/>", () => {
+    it("does not renders the dashboard chart", () => {
         const wrapper = shallow(<DashboardCharts role="fake role" />);
         expect(wrapper.find(BarChart)).toHaveLength(0);
     });
 
-    it("renders the Immigration Officer dashboard when it is logged in appropriately, there is a chart and a card", () => {
+    it("renders the Immigration Officer dashboard chart when it is logged in appropriately, there is a chart and a card", () => {
         const wrapper = shallow(<DashboardCharts role={USER_ROLES.iOfficer} data={[]} />);
         expect(wrapper.find(BarChart)).toHaveLength(1);
         expect(wrapper.find(Bar)).toHaveLength(2);
