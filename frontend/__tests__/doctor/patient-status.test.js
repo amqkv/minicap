00c3d88670ad6c40f4b9fec9f5c22e05a187568a
raisa61 <raisazaman2000@gmail.com>
2022-03-10 12:38:14 -0500

    modified the status test

--- frontend/__tests__/doctor/patient-status.test.js
+++ frontend/__tests__/doctor/patient-status.test.js
@@ -5,13 +5,14 @@ import { Box, Text } from "@chakra-ui/react";
 
 describe("<PatientStatus/>", () => {
     it("Renders all the Box components", () => {
-        const wrapper = shallow(<PatientStatus patient={DEFAULT_PATIENT} />);
+        const wrapper = shallow(<PatientStatus patient={DEFAULT_PATIENT} statusIndex={0} />);
         expect(wrapper.find(Box)).toHaveLength(1);
     });
     it("Renders all the Text components", () => {
         let patient = { ...DEFAULT_PATIENT };
         patient.requiredDetails.weight = true;
-        const wrapper = shallow(<PatientStatus patient={patient} />);
+        patient.status[0].weight.value = 0;
+        const wrapper = shallow(<PatientStatus patient={patient} statusIndex={0} />);
         expect(wrapper.find(Text)).toHaveLength(1);
     });
 });
