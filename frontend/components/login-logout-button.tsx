8ae1fde8d461e11c970b779e180eeafd6398d5c9
NicholasYip <yiphoiye.n@gmail.com>
2022-03-15 18:20:07 -0400

    fixed logout redirect

--- frontend/components/login-logout-button.tsx
+++ frontend/components/login-logout-button.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@chakra-ui/react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { MAIN_COLOR } from "@frontend/utils/constants";
+import { useRouter } from "next/router";
 
 function LoginLogoutButton({}) {
     const { data: session } = useSession();
+    const router = useRouter();
+
+    function signOutRedirect() {
+        signOut();
+        router.push("/");
+    }
 
     return (
         <Button
@@ -11,7 +18,7 @@ function LoginLogoutButton({}) {
             _hover={{ opacity: "90%" }}
             size="sm"
             color="white"
-            onClick={() => (session ? signOut() : signIn())}>
+            onClick={() => (session ? signOutRedirect() : signIn())}>
             {session ? "Sign Out" : "Get Started Here"}
         </Button>
     );
