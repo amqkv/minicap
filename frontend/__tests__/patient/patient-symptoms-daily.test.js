56416831e2f112315ab87946ae70645ab290dc58
Olivier <olivier_phan@outlook.com>
2022-03-14 20:43:47 -0400

    SOEN-10: Adding new tests

--- frontend/__tests__/patient/patient-symptoms-daily.test.js
+++ frontend/__tests__/patient/patient-symptoms-daily.test.js
@@ -1,5 +1,5 @@
 import { shallow } from "enzyme";
-import { Button, Heading } from "@chakra-ui/react";
+import { Button, Heading, Box } from "@chakra-ui/react";
 import PatientFormsToFill from "@frontend/components/forms/patient-form-to-fill";
 import { DEFAULT_REQUIRED_DETAILS, DEFAULT_PAST_CONDITIONS } from "../__mock__/mock";
 import { useSession } from "next-auth/react";
@@ -8,20 +8,59 @@ import { USER_ROLES } from "@frontend/utils/constants";
 jest.mock("next-auth/react");
 
 describe("test patient's form", () => {
-    it("renders a <PatientFormsToFill /> component", () => {
+    beforeEach(() => {
         useSession.mockReturnValue({
             data: {
-              user: {
-                Role: USER_ROLES.patient,
-              },
+                user: {
+                    Role: USER_ROLES.patient,
+					AccountId: 0
+                },
             },
-          });
+        });
+    });
 
+    it("renders a <PatientFormsToFill /> component", () => {
         const wrapper = shallow(
             <PatientFormsToFill requiredDetails={DEFAULT_REQUIRED_DETAILS} pastConditions={DEFAULT_PAST_CONDITIONS} />
         );
         expect(wrapper.find(Heading)).toHaveLength(4);
         expect(wrapper.find(Button)).toHaveLength(1);
+    });
 
+    it("activates the onSubmit with Errors in form", () => {
+        const mockEvent = {
+            preventDefault: () => {},
+            target: [{ value: "temperature" }, { value: "weight" }, { value: "symptoms" }],
+        };
+        const component = shallow(
+            <PatientFormsToFill requiredDetails={DEFAULT_REQUIRED_DETAILS} pastConditions={DEFAULT_PAST_CONDITIONS} />
+        );
+        //component.setProps();
+        component.find("form").simulate("submit", mockEvent);
+        component.update();
+    });
+	it("activates the onSubmit with fields incomplete in form", () => {
+        const mockEvent = {
+            preventDefault: () => {},
+            target: [{ value: 3 }, { value: 3 }, { value: ""}],
+        };
+        const component = shallow(
+            <PatientFormsToFill requiredDetails={DEFAULT_REQUIRED_DETAILS} pastConditions={DEFAULT_PAST_CONDITIONS} />
+        );
+        //component.setProps();
+        component.find("form").simulate("submit", mockEvent);
+        component.update();
+    });
+	it("activates the onSubmit with no errors in form", () => {
+        const mockEvent = {
+            preventDefault: () => {},
+            target: [{ value: 3 }, { value: 3 }, { value: "symptoms" }],
+        };
+        const component = shallow(
+            <PatientFormsToFill requiredDetails={DEFAULT_REQUIRED_DETAILS} pastConditions={DEFAULT_PAST_CONDITIONS} />
+        );
+        //component.setProps();
+        component.find("form").simulate("submit", mockEvent);
+        component.update();
     });
 });
