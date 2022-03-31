318e41a9456b73ceee38a881b91fa366983b370d
Be An <amqkvo@gmail.com>
2022-03-31 15:34:54 -0400

    Addressed PR comments + changed calendar times to 24h format

--- frontend/components/doctor/appointment-calendar.tsx
+++ frontend/components/doctor/appointment-calendar.tsx
@@ -1,13 +1,19 @@
 import { Box } from "@chakra-ui/react";
 import { Appointment } from "@frontend/models/appointment";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, Culture, DateLocalizer, DateRange, Formats, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { transformToEvent } from "@frontend/functions/transform-to-calendar-event";
+import { useMemo } from "react";
 
 const localizer = momentLocalizer(moment);
 export default function AppointmentCalendar({ appointmentList }: { appointmentList: Appointment[] }) {
     const today = new Date();
+    const formats = {
+        eventTimeRangeFormat: ({ start, end }: { start: Date; end: Date }, culture: any) =>
+            `${localizer.format(start, "HH:mm", culture)} – ${localizer.format(end, "HH:mm", culture)}`,
+        timeGutterFormat: (date: Date, culture: any) => localizer.format(date, "HH:mm", culture),
+    };
 
     return (
         <Box mt={10} mr={16}>
@@ -15,6 +21,7 @@ export default function AppointmentCalendar({ appointmentList }: { appointmentLi
                 localizer={localizer}
                 events={transformToEvent(appointmentList)}
                 defaultView="week"
+                formats={formats}
                 defaultDate={today}
                 style={{ height: "650px", width: "1050px" }}
                 min={new Date(today.getFullYear(), today.getMonth(), today.getDate(), 8, 30)}
