7cae13b408f0f29f9f561803108ed30a7b4819b5
Olivier <olivier_phan@outlook.com>
2022-04-01 20:18:32 -0400

    SOEN-158: Backend Admin Tests + New constants

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,25 +1,37 @@
 const ROLE = {
     ADMIN: "Admin",
     DOCTOR: "Doctor",
-    HEALTH_OFFICIAL: "HealthOfficial",
+    HEALTH_OFFICIAL: "Health Official",
     IMMIGRATION_OFFICER: "ImmigrationOfficer",
     PATIENT: "Patient",
 };
 
 const TEST_CONSTANTS = {
-    // admin account
     TESTER_ADMIN: {
-        AccountId: "17", // this is nicolas@gmail.com in the DB, DON'T DELETE IT
+        AccountId: "78", // this is sitetester@email.com in the DB, DON'T DELETE IT
         Role: "Admin",
     },
+    HEALTH_OFFICIAL_ACCOUNT: {
+        AccountId: "1190", // constant-healthofficial@dontmodify.com
+        Role: "Health Official",
+    },
+    IMMIGRATION_OFFICER_ACCOUNT: {
+        AccountId: "1216", // constant-immigration@dontmodify.com
+        Role: "ImmigrationOfficer",
+    },
+    DOCTOR_ASSIGN_ADMIN_TEST: {
+        Doctor_Id: "19", // user id 1197 constant-doctor4admin@dontmodify.com
+        Role: "Doctor",
+        Patient_Id: "590", // user id 1198 constant-patient-unassigned@dontmodify.com
+    },
     UNCONFIRMED_ACCOUNT: {
-        AccountId: "1", // this is test@test.com
+        AccountId: "1215", // forever-unconfirmed@dontmodify.com
     },
     PATIENT_ACCOUNT: {
-        AccountId: "51",
+        AccountId: "1201", // constant-patient-main@dontmodify.com
     },
     UPDATE_ROLE_ACCOUNT: {
-        AccountId: "828",
+        AccountId: "828", // jfupdatetest@test.com
     },
     REJECTED_ACCOUNT: {
         AccountId: "859",
@@ -28,6 +40,35 @@ const TEST_CONSTANTS = {
     STATUS_TEST_ID: 2,
     // Patient for the review test
     STATUS_PATIENT_ID: 27,
+    INVALID_ACCOUNT_ID: "0",
+};
+
+const DOCTOR_TEST_SUBJECTS = {
+    DOCTOR_INFO: {
+        AccountId: "1192", // constant-doctor@dontmodify.com
+        Role: "Doctor",
+        Doctor_Id: "18",
+    },
+    PATIENT_ONE: {
+        AccountId: "1193",
+        Patient_Id: "585",
+        Doctor_DoctorId: "18",
+    },
+    PATIENT_TWO: {
+        AccountId: "1194",
+        Patient_Id: "586",
+        Doctor_DoctorId: "18",
+    },
+    PATIENT_THREE: {
+        AccountId: "1195",
+        Patient_Id: "587",
+        Doctor_DoctorId: "18",
+    },
+    PATIENT_FOUR: {
+        AccountId: "1196",
+        Patient_Id: "588",
+        Doctor_DoctorId: "18",
+    },
 };
 
 // Email used to send rejection to rejected users
@@ -49,4 +90,5 @@ module.exports = {
     BOOLEANS,
     MOMENT_TIMEZONE_ADJUSTMENT,
     ADMIN_EMAIL_ACCOUNT,
+    DOCTOR_TEST_SUBJECTS,
 };
