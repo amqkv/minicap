f140fd1e44764a22cd92389bab59944bf68bde7d
Kelvin <kelvintrang2000@gmail.com>
2022-04-05 15:36:37 -0400

    added test

--- frontend/__tests__/homepage/confirm-appointment.test.js
+++ frontend/__tests__/homepage/confirm-appointment.test.js
@@ -18,7 +18,7 @@ afterAll(() => {
 const mock_appointment = [
     {
         appointmentId:23,
-        date: "2022-07-27",
+        date: "2023-07-27",
         time: "10:30 to 11:00",
     },
 ]
@@ -36,15 +36,19 @@ describe("Rendering <PatientDetailsToProvideForm/>", () => {
 // TOFIX: unknown test failure. 
 describe("onConfirm", () => {
     it("Calls fetch", async () => {
-        const component = shallow(<ConfirmAppointment  appointment={mock_appointment}/>);
-        component.find(Button).at(0).simulate("click");
+        const wrapper = shallow(<ConfirmAppointment  appointment={mock_appointment}/>);
+        expect(wrapper.find(Button)).toHaveLength(2);
+        expect(wrapper.find(Text).at(0).text()).toBe("Nov 30 1899 from ");
+        wrapper.find(Button).at(0).simulate("click");
     });
 });
 
 describe("onDecline", () => {
     it("Calls fetch", async () => {
-        const component = shallow(<ConfirmAppointment  appointment={mock_appointment}/>);
-        component.find(Button).at(1).simulate("click");
+        const wrapper = shallow(<ConfirmAppointment  appointment={mock_appointment}/>);
+        wrapper.find(Button).at(1).simulate("click");
+        expect(wrapper.find(Button)).toHaveLength(2);
+        expect(wrapper.find(Text).at(0).text()).toBe("Nov 30 1899 from ");
     });
 });
 
